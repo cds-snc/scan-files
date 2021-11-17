5c1559633cb2ffbd5c9bfe7ff53fe23c11d34282
Mohamed Nur <mohamed.nur@cds-snc.ca>
2021-11-17 14:40:04 -0500

    chore: pull in module version that fixes conftest warnings

--- terragrunt/aws/api/vpc.tf
+++ terragrunt/aws/api/vpc.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source            = "github.com/cds-snc/terraform-modules?ref=v0.0.43//vpc"
+  source            = "github.com/cds-snc/terraform-modules?ref=v0.0.46//vpc"
   name              = var.product_name
   billing_tag_value = var.billing_code
   high_availability = true
