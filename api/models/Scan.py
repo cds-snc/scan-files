d1d6b094be32ba7bcc397b740e369480cc50c48d
Mohamed Nur <mohamed.nur@cds-snc.ca>
2021-11-18 17:37:53 -0500

    feat: assemblyline file upload and scan status retrieval

--- api/models/Scan.py
+++ api/models/Scan.py
@@ -1,10 +1,22 @@
 import datetime
 import uuid
 
+from enum import Enum
 from models import Base
 from sqlalchemy import DateTime, Column, String
 from sqlalchemy.dialects.postgresql import JSONB, UUID
 from sqlalchemy.sql.sqltypes import Numeric
+
+
+class ScanProviders(Enum):
+    ASSEMBLYLINE = "Assemblyline"
+
+
+class ScanVerdicts(Enum):
+    IN_PROGRESS = "in_progress"
+    CLEAN = "clean"
+    MALICIOUS = "malicious"
+    ERROR = "error"
 
 
 class Scan(Base):
@@ -12,12 +24,12 @@ class Scan(Base):
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     file_name = Column(String, nullable=False)
-    file_size = Column(Numeric, nullable=False)
-    save_path = Column(String, nullable=False)
-    sha256 = Column(String, nullable=False)
     scan_provider = Column(String, nullable=False)
-    submitter = Column(String, nullable=False)
 
+    submitter = Column(String, nullable=True)
+    file_size = Column(Numeric, nullable=True)
+    save_path = Column(String, nullable=True)
+    sha256 = Column(String, nullable=True)
     verdict = Column(String, nullable=True)
     quarantine_path = Column(String, nullable=True)
     meta_data = Column(JSONB, nullable=True)
--- api/tests/factories.py
+++ api/tests/factories.py
@@ -1,6 +1,7 @@
 import factory
 
 from models.Scan import Scan
+from random import randrange
 
 # When adding new factories ensure you add the factory to the conftest session fixture so that they can be linked to the test db session
 
@@ -10,13 +11,12 @@ class ScanFactory(factory.alchemy.SQLAlchemyModelFactory):
         model = Scan
 
     id = factory.Faker("uuid4")
-    name = factory.Faker("name")
-    file_name = (factory.Faker("file_name"),)
-    file_size = (factory.Faker("number"),)
-    save_path = ("s3://foo.bar/baz",)
-    sha256 = (factory.Faker("sha256"),)
-    scan_provider = (factory.Faker("buzzword"),)
-    submitter = (factory.Faker("name"),)
-    verdict = ("clean",)
-    quarantine_path = ("s3://foo.bar/fizz",)
+    file_name = factory.Faker("file_name")
+    file_size = randrange(100)
+    save_path = "s3://foo.bar/baz"
+    sha256 = factory.Faker("sha256")
+    scan_provider = factory.Faker("company")
+    submitter = factory.Faker("name")
+    verdict = "clean"
+    quarantine_path = "s3://foo.bar/fizz"
     meta_data = {}
