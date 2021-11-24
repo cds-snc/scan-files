0cd45be16608c86719f0dbc552953d3425baa748
Mohamed Nur <mohamed.nur@cds-snc.ca>
2021-11-24 12:35:37 -0500

    feat: output api lambda variables for future component

--- terragrunt/aws/api/outputs.tf
+++ terragrunt/aws/api/outputs.tf
@@ -9,3 +9,15 @@ output "private_subnet_ids" {
 output "log_bucket_id" {
   value = module.log_bucket.s3_bucket_id
 }
+
+output "function_arn" {
+  value = module.api.function_arn
+}
+
+output "function_name" {
+  value = module.api.function_name
+}
+
+output "invoke_arn" {
+  value = module.api.invoke_arn
+}
