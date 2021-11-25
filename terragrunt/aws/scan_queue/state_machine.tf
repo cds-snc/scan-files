884a0b508884b71ea702af39da51e46cf1d7c117
Mohamed Nur <mohamed.nur@cds-snc.ca>
2021-11-25 12:56:27 -0500

    chore: state machine that will cleanup orphaned locks

--- terragrunt/aws/scan_queue/state_machine.tf
+++ terragrunt/aws/scan_queue/state_machine.tf
@@ -18,3 +18,22 @@ data "template_file" "scan_queue" {
     concurrent_scan_limit = var.concurrent_scan_limit
   }
 }
+
+resource "aws_sfn_state_machine" "scan_queue_lock_cleanup" {
+  name     = "assemblyline-file-scan-queue-lock-cleanup"
+  role_arn = aws_iam_role.scan_queue.arn
+
+  definition = data.template_file.scan_queue_lock_cleanup.rendered
+
+  tags = {
+    CostCentre = var.billing_code
+    Terraform  = true
+  }
+}
+data "template_file" "scan_queue_lock_cleanup" {
+  template = file("state-machines/scan-queue-lock-cleanup.json")
+  vars = {
+    table_semaphore = aws_dynamodb_table.scan-locktable.name
+    lock_name       = "ScanSemaphore"
+  }
+}
