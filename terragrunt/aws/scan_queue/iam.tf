575130293b0a24109d946222c3af4936652235ff
Mohamed Nur <85885638+mohamed-cds@users.noreply.github.com>
2021-11-30 10:13:36 -0500

    feat: allow eventbridge to assume the scanqueue role (#34)

--- terragrunt/aws/scan_queue/iam.tf
+++ terragrunt/aws/scan_queue/iam.tf
@@ -13,6 +13,11 @@ data "aws_iam_policy_document" "service_principal" {
       type        = "Service"
       identifiers = ["states.${var.region}.amazonaws.com"]
     }
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
   }
 }
 
