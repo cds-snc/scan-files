e12a501c5dfc22f866baf6b227cf949619c469a6
Pat Heard <patrick.heard@cds-snc.ca>
2022-06-24 18:19:21 +0000

    chore: adjust input descriptions
    Really, this is just me forcing a `terraform apply` to run
    on the `scan_queue` module so that we stop seeing the
    function name flip flop.

--- terragrunt/aws/scan_queue/inputs.tf
+++ terragrunt/aws/scan_queue/inputs.tf
@@ -1,31 +1,35 @@
 variable "concurrent_scan_limit" {
-  type        = number
   description = "The maximum number of ongoing scans"
+  type        = number
 }
 
 variable "retry_interval_seconds" {
-  type        = number
   description = "The number of seconds to wait before polling for results"
+  type        = number
 }
 
 variable "api_function_arn" {
-  type = string
+  default = "ARN of the API function"
+  type    = string
 }
 
 variable "api_function_name" {
-  type = string
+  default = "Name of the API function"
+  type    = string
 }
 
 variable "locktable_name" {
-  type        = string
   description = "Scan queue lock semaphore Dynamodb table name"
+  type        = string
 }
 
 variable "completed_scans_table_name" {
-  type = string
+  description = "Complete scan Dynamodb table name"
+  type        = string
 }
 
 variable "scan_queue_statemachine_name" {
-  type = string
+  description = "Name of the scan queue state machine"
+  type        = string
 }
 
