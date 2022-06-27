7951f8348856c5712ec3ec4ef071717742062709
Pat Heard <patrick.heard@cds-snc.ca>
2022-06-27 10:14:43 -0400

    feat: add S3 scan object lambda (#122)
    Add the lambda that will be used by products to trigger file scans
    on S3 object creation events and update the tags of the S3
    object with the scan results.
    
    This lambda will be managed using a Terraform Module installed in
    product accounts.

---
+++ module/s3-scan-object/app.js
@@ -0,0 +1,243 @@
+"use strict";
+
+/**
+ * Lambda function that can be invoked by S3 events or SNS messages.
+ * This function triggers a scan of newly created S3 objects or updates
+ * the scan status of existing S3 objects based on an SNS message payload.
+ */
+
+const axios = require("axios");
+const util = require("util");
+const { S3Client, PutObjectTaggingCommand } = require("@aws-sdk/client-s3");
+const { SSMClient, GetParameterCommand } = require("@aws-sdk/client-ssm");
+
+const AWS_ACCOUNT_ID = process.env.AWS_ACCOUNT_ID;
+const REGION = process.env.REGION;
+const ENDPOINT_URL = process.env.AWS_SAM_LOCAL ? "http://host.docker.internal:3001" : undefined;
+const SCAN_FILES_URL = process.env.SCAN_FILES_URL;
+const SCAN_FILES_API_KEY_PARAM_NAME = process.env.SCAN_FILES_API_KEY_PARAM_NAME;
+const SCAN_IN_PROGRESS = "in_progress";
+const SCAN_FAILED_TO_START = "failed_to_start";
+const SNS_SCAN_COMPLETE_TOPIC_ARN = process.env.SNS_SCAN_COMPLETE_TOPIC_ARN;
+const EVENT_S3 = "aws:s3";
+const EVENT_SNS = "aws:sns";
+
+const s3Client = new S3Client({ region: REGION, endpoint: ENDPOINT_URL });
+const ssmClient = new SSMClient({ region: REGION, endpoint: ENDPOINT_URL });
+
+/**
+ * Performs function initialization outside of the Lambda handler so that
+ * it only occurs once per cold start of the function rather than on
+ * every invocation.
+ * @returns {Promise<{apiKey: string}>} API key to use for the scan
+ */
+const initConfig = async () => {
+  return (async () => {
+    try {
+      const command = new GetParameterCommand({
+        Name: SCAN_FILES_API_KEY_PARAM_NAME,
+        WithDecryption: true,
+      });
+      const response = await ssmClient.send(command);
+      return { apiKey: response.Parameter.Value };
+    } catch (error) {
+      console.error(`Unable to get '${SCAN_FILES_API_KEY_PARAM_NAME}' parameter: ${error}`);
+      throw error;
+    }
+  })();
+};
+
+// Start config load on cold starts. This can switch to a top level `await` if we switch to ES modules:
+// https://aws.amazon.com/blogs/compute/using-node-js-es-modules-and-top-level-await-in-aws-lambda/
+const configPromise = initConfig();
+
+/**
+ * Lambda handler function.  This function is invoked when a new S3 object is
+ * created in response to `s3:ObjectCreated:*` events or when an SNS message
+ * is received with an update scan status.
+ * @param {Object} event Lambda invocation event
+ */
+exports.handler = async (event) => {
+  const config = await configPromise;
+  let errorCount = 0;
+
+  // Process all event records
+  for (const record of event.Records) {
+    let scanStatus = null;
+    let isObjectTagged = false;
+    let eventSource = getRecordEventSource(record);
+    let s3Object = getS3ObjectFromRecord(eventSource, record);
+
+    // Start a scan of the new S3 object
+    if (eventSource !== null && s3Object !== null) {
+      if (eventSource === EVENT_S3) {
+        const response = await startS3ObjectScan(
+          `${SCAN_FILES_URL}/clamav/s3`,
+          config.apiKey,
+          s3Object,
+          AWS_ACCOUNT_ID,
+          SNS_SCAN_COMPLETE_TOPIC_ARN
+        );
+        scanStatus =
+          response !== undefined && response.status === 200
+            ? SCAN_IN_PROGRESS
+            : SCAN_FAILED_TO_START;
+
+        // Get the scan status for an existing S3 object
+      } else if (eventSource === EVENT_SNS) {
+        scanStatus = record.Sns.MessageAttributes["av-status"].Value;
+      }
+    } else {
+      console.error(`Unsupported event record: ${util.inspect(record)}`);
+    }
+
+    // Tag the S3 object if we've got a scan status
+    if (scanStatus !== null) {
+      isObjectTagged = await tagS3Object(s3Client, s3Object, [
+        { Key: "av-scanner", Value: "clamav" },
+        { Key: "av-status", Value: scanStatus },
+        { Key: "av-timestamp", Value: new Date().getTime() },
+      ]);
+    }
+
+    // Track if there were any errors processing this record
+    if (scanStatus === SCAN_FAILED_TO_START || isObjectTagged === false) {
+      errorCount++;
+    }
+  }
+
+  return {
+    status: errorCount > 0 ? 422 : 200,
+    body: `Event records processesed: ${event.Records.length}, Errors: ${errorCount}`,
+  };
+};
+
+/**
+ * Determines the event record's source service.  This is either S3 or SNS.
+ * @param {Object} record Lambda invocation event record
+ * @returns {String} Event record source service, or `null` if not valid
+ */
+const getRecordEventSource = (record) => {
+  let eventSource = null;
+
+  if (record.eventSource === EVENT_S3) {
+    eventSource = EVENT_S3;
+  } else if (record.EventSource === EVENT_SNS) {
+    eventSource = EVENT_SNS;
+  }
+
+  return eventSource;
+};
+
+/**
+ * Retrieves the S3 object's Bucket and key from the Lambda invocation event.
+ * @param {String} eventSource The source of the event record
+ * @param {Object} record Lambda invocation event record
+ * @returns {{Bucket: string, Key: string}} S3 object bucket and key
+ */
+const getS3ObjectFromRecord = (eventSource, record) => {
+  let s3Object = null;
+
+  if (eventSource === EVENT_S3) {
+    s3Object = {
+      Bucket: record.s3.bucket.name,
+      Key: decodeURIComponent(record.s3.object.key.replace(/\+/g, " ")),
+    };
+  } else if (eventSource === EVENT_SNS) {
+    const s3ObjectUrl = record.Sns.MessageAttributes["av-filepath"].Value;
+    if (s3ObjectUrl && s3ObjectUrl.startsWith("s3://")) {
+      s3Object = parseS3Url(s3ObjectUrl);
+    }
+  }
+
+  return s3Object;
+};
+
+/**
+ * Given an S3 object URL, parses the URL and returns the S3 object's bucket and key.
+ * @param {String} url S3 object URL in format `s3://bucket/key`
+ * @returns {{Bucket: string, Key: string}} S3 object bucket and key or null
+ */
+const parseS3Url = (url) => {
+  let s3Object = null;
+
+  const parsedUrl = url ? url.match(/s3:\/\/([^/]+)\/(.+)/) : null;
+  if (parsedUrl !== null && parsedUrl.length === 3) {
+    s3Object = {
+      Bucket: parsedUrl[1],
+      Key: parsedUrl[2],
+    };
+  }
+
+  return s3Object;
+};
+
+/**
+ * Starts a scan of the S3 object using the provided URL endpoint and API key.
+ * @param {String} apiEndpoint API endpoint to use for the scan
+ * @param {String} apiKey API authorization key to use for the scan
+ * @param {{Bucket: string, Key: string}} s3Object S3 object to tag
+ * @param {String} awsAccountId AWS account ID to use for the scan
+ * @param {String} snsArn ARN of the SNS topic to publish scan results to
+ * @returns {Response} Axios response from the scan request
+ */
+const startS3ObjectScan = async (apiEndpoint, apiKey, s3Object, awsAccountId, snsArn) => {
+  try {
+    const response = await axios.post(
+      apiEndpoint,
+      {
+        aws_account: awsAccountId,
+        s3_key: `s3://${s3Object.Bucket}/${s3Object.Key}`,
+        sns_arn: snsArn,
+      },
+      {
+        headers: {
+          Accept: "application/json",
+          Authorization: apiKey,
+        },
+      }
+    );
+    console.info(`Scan response ${response.status}: ${util.inspect(response.data)}`);
+    return response;
+  } catch (error) {
+    console.error(
+      `Could not start scan for ${util.inspect(s3Object)}: ${util.inspect(error.response)}`
+    );
+    return error.response;
+  }
+};
+
+/**
+ * Tags the S3 object with the provided tags.
+ * @param {S3Client} s3Client AWS SDK S3 client used to tag the object
+ * @param {{Bucket: string, Key: string}} s3Object S3 object to tag
+ * @param {Array<{Key: string, Value: string}>} tags Array of Key/Value pairs to tag the S3 object with
+ */
+const tagS3Object = async (s3Client, s3Object, tags) => {
+  const tagging = {
+    Tagging: {
+      TagSet: tags,
+    },
+  };
+  let isSuccess = false;
+
+  try {
+    const command = new PutObjectTaggingCommand({ ...s3Object, ...tagging });
+    const response = await s3Client.send(command);
+    isSuccess = response.VersionId !== undefined;
+  } catch (error) {
+    console.error(`Failed to tag S3 object: ${error}`);
+  }
+
+  return isSuccess;
+};
+
+// Helpers for testing
+exports.helpers = {
+  getRecordEventSource,
+  getS3ObjectFromRecord,
+  initConfig,
+  parseS3Url,
+  startS3ObjectScan,
+  tagS3Object,
+};
---
+++ module/s3-scan-object/app.test.js
@@ -0,0 +1,359 @@
+"use strict";
+
+const axios = require("axios");
+const { mockClient } = require("aws-sdk-client-mock");
+const { S3Client, PutObjectTaggingCommand } = require("@aws-sdk/client-s3");
+const { SSMClient, GetParameterCommand } = require("@aws-sdk/client-ssm");
+
+const mockS3Client = mockClient(S3Client);
+const mockSSMClient = mockClient(SSMClient);
+mockSSMClient.on(GetParameterCommand).resolves({
+  Parameter: { Value: "someSuperSecretValue" },
+});
+
+const { handler, helpers } = require("./app.js");
+const {
+  getRecordEventSource,
+  getS3ObjectFromRecord,
+  initConfig,
+  parseS3Url,
+  startS3ObjectScan,
+  tagS3Object,
+} = helpers;
+
+jest.mock("axios");
+global.console = {
+  ...console,
+  error: jest.fn(),
+  info: jest.fn(),
+};
+
+const TEST_TIME = new Date(1978, 3, 30).getTime();
+beforeAll(() => {
+  jest.useFakeTimers().setSystemTime(TEST_TIME);
+});
+
+beforeEach(() => {
+  jest.resetAllMocks();
+  mockS3Client.reset();
+  mockSSMClient.reset();
+});
+
+describe("handler", () => {
+  test("records success", async () => {
+    const event = {
+      Records: [
+        {
+          eventSource: "aws:s3",
+          s3: {
+            bucket: { name: "foo" },
+            object: { key: "bar" },
+          },
+        },
+        {
+          EventSource: "aws:sns",
+          Sns: {
+            MessageAttributes: {
+              "av-filepath": { Value: "s3://bam/baz" },
+              "av-status": { Value: "SPIFY" },
+            },
+          },
+        },
+      ],
+    };
+    const expectedResponse = {
+      status: 200,
+      body: "Event records processesed: 2, Errors: 0",
+    };
+
+    axios.post.mockResolvedValue({ status: 200 });
+    mockS3Client.on(PutObjectTaggingCommand).resolves({ VersionId: "yeet" });
+
+    const response = await handler(event);
+    expect(response).toEqual(expectedResponse);
+    expect(mockS3Client).toHaveReceivedNthCommandWith(1, PutObjectTaggingCommand, {
+      Bucket: "foo",
+      Key: "bar",
+      Tagging: {
+        TagSet: [
+          { Key: "av-scanner", Value: "clamav" },
+          { Key: "av-status", Value: "in_progress" },
+          { Key: "av-timestamp", Value: TEST_TIME },
+        ],
+      },
+    });
+    expect(mockS3Client).toHaveReceivedNthCommandWith(2, PutObjectTaggingCommand, {
+      Bucket: "bam",
+      Key: "baz",
+      Tagging: {
+        TagSet: [
+          { Key: "av-scanner", Value: "clamav" },
+          { Key: "av-status", Value: "SPIFY" },
+          { Key: "av-timestamp", Value: TEST_TIME },
+        ],
+      },
+    });
+  });
+
+  test("records failed, failed to start", async () => {
+    const event = {
+      Records: [
+        {
+          eventSource: "aws:s3",
+          s3: {
+            bucket: { name: "foo" },
+            object: { key: "bar" },
+          },
+        },
+      ],
+    };
+    const expectedResponse = {
+      status: 422,
+      body: "Event records processesed: 1, Errors: 1",
+    };
+
+    axios.post.mockResolvedValue({ status: 500 });
+    mockS3Client.on(PutObjectTaggingCommand).resolves({ VersionId: "yeet" });
+
+    const response = await handler(event);
+    expect(response).toEqual(expectedResponse);
+    expect(mockS3Client).toHaveReceivedNthCommandWith(1, PutObjectTaggingCommand, {
+      Bucket: "foo",
+      Key: "bar",
+      Tagging: {
+        TagSet: [
+          { Key: "av-scanner", Value: "clamav" },
+          { Key: "av-status", Value: "failed_to_start" },
+          { Key: "av-timestamp", Value: TEST_TIME },
+        ],
+      },
+    });
+  });
+
+  test("records failed, undefined reponse", async () => {
+    const event = {
+      Records: [
+        {
+          eventSource: "aws:s3",
+          s3: {
+            bucket: { name: "foo" },
+            object: { key: "bar" },
+          },
+        },
+      ],
+    };
+    const expectedResponse = {
+      status: 422,
+      body: "Event records processesed: 1, Errors: 1",
+    };
+
+    axios.post.mockResolvedValue(undefined);
+    mockS3Client.on(PutObjectTaggingCommand).resolves({ VersionId: "yeet" });
+
+    const response = await handler(event);
+    expect(response).toEqual(expectedResponse);
+    expect(mockS3Client).toHaveReceivedNthCommandWith(1, PutObjectTaggingCommand, {
+      Bucket: "foo",
+      Key: "bar",
+      Tagging: {
+        TagSet: [
+          { Key: "av-scanner", Value: "clamav" },
+          { Key: "av-status", Value: "failed_to_start" },
+          { Key: "av-timestamp", Value: TEST_TIME },
+        ],
+      },
+    });
+  });
+
+  test("records failed, invalid event source", async () => {
+    const event = {
+      Records: [
+        {
+          eventSource: "muffins",
+        },
+      ],
+    };
+    const expectedResponse = {
+      status: 422,
+      body: "Event records processesed: 1, Errors: 1",
+    };
+
+    axios.post.mockResolvedValue({ status: 200 });
+    mockS3Client.on(PutObjectTaggingCommand).resolves({ VersionId: "yeet" });
+
+    const response = await handler(event);
+    expect(response).toEqual(expectedResponse);
+  });
+});
+
+describe("getRecordEventSource", () => {
+  test("valid event sources", () => {
+    expect(getRecordEventSource({ eventSource: "aws:s3" })).toBe("aws:s3");
+    expect(getRecordEventSource({ EventSource: "aws:sns" })).toBe("aws:sns");
+  });
+
+  test("invalid event sources", () => {
+    expect(getRecordEventSource({ eventSource: "aws:s3:ca-central-1" })).toBe(null);
+    expect(getRecordEventSource({ EventSource: "aws:s3" })).toBe(null);
+    expect(getRecordEventSource({ eventSource: "aws:sns" })).toBe(null);
+    expect(getRecordEventSource({ eventSource: "pohtaytoes" })).toBe(null);
+    expect(getRecordEventSource({})).toBe(null);
+  });
+});
+
+describe("getS3ObjectFromRecord", () => {
+  test("s3 event", () => {
+    const record = {
+      s3: {
+        bucket: {
+          name: "foo",
+        },
+        object: {
+          key: encodeURIComponent("some-folder-path/this is the file name"),
+        },
+      },
+    };
+    const expected = {
+      Bucket: "foo",
+      Key: "some-folder-path/this is the file name",
+    };
+    expect(getS3ObjectFromRecord("aws:s3", record)).toEqual(expected);
+  });
+
+  test("sns event", () => {
+    const record = {
+      Sns: {
+        MessageAttributes: {
+          "av-filepath": {
+            Value: "s3://bar/bam",
+          },
+        },
+      },
+    };
+    const expected = {
+      Bucket: "bar",
+      Key: "bam",
+    };
+    expect(getS3ObjectFromRecord("aws:sns", record)).toEqual(expected);
+  });
+
+  test("sns event, invalid av-filepath", () => {
+    const record = {
+      Sns: {
+        MessageAttributes: {
+          "av-filepath": {
+            Value: "file:///some.gif",
+          },
+        },
+      },
+    };
+    expect(getS3ObjectFromRecord("aws:sns", record)).toBe(null);
+  });
+
+  test("invalid event", () => {
+    expect(getS3ObjectFromRecord("muffins", {})).toBe(null);
+  });
+});
+
+describe("initConfig", () => {
+  test("retrieves the config value", async () => {
+    mockSSMClient.on(GetParameterCommand).resolvesOnce({
+      Parameter: { Value: "anotherEquallySecretValue" },
+    });
+
+    const config = await initConfig();
+    expect(config).toEqual({ apiKey: "anotherEquallySecretValue" });
+  });
+
+  test("throws an error on failure", async () => {
+    mockSSMClient.on(GetParameterCommand).rejectsOnce(new Error("nope"));
+    await expect(initConfig()).rejects.toThrow("nope");
+  });
+});
+
+describe("parseS3Url", () => {
+  test("successful parse", async () => {
+    expect(parseS3Url("s3://foo/bar")).toEqual({ Bucket: "foo", Key: "bar" });
+    expect(parseS3Url("s3://the-spice-must-flow/bar/bam/baz/bing.png")).toEqual({
+      Bucket: "the-spice-must-flow",
+      Key: "bar/bam/baz/bing.png",
+    });
+  });
+
+  test("unsuccessful parse", async () => {
+    expect(parseS3Url(undefined)).toBe(null);
+    expect(parseS3Url(null)).toBe(null);
+    expect(parseS3Url("")).toBe(null);
+    expect(parseS3Url("s3://foo")).toBe(null);
+  });
+});
+
+describe("startS3ObjectScan", () => {
+  test("starts a scan", async () => {
+    axios.post.mockResolvedValueOnce({ status: 200 });
+    const response = await startS3ObjectScan(
+      "http://somedomain.com",
+      "someSuperSecretValue",
+      {
+        Bucket: "foo",
+        Key: "bar",
+      },
+      "123456789012",
+      "someSnsTopicArn"
+    );
+    expect(response).toEqual({ status: 200 });
+    expect(axios.post.mock.calls[0]).toEqual([
+      "http://somedomain.com",
+      {
+        aws_account: "123456789012",
+        s3_key: "s3://foo/bar",
+        sns_arn: "someSnsTopicArn",
+      },
+      {
+        headers: {
+          Accept: "application/json",
+          Authorization: "someSuperSecretValue",
+        },
+      },
+    ]);
+  });
+
+  test("fails to start a scan", async () => {
+    axios.post.mockRejectedValueOnce({ response: { status: 500 } });
+    const response = await startS3ObjectScan(
+      "http://somedomain.com",
+      "someSuperSecretValue",
+      {
+        Bucket: "foo",
+        Key: "bar",
+      },
+      "123456789012",
+      "someSnsTopicArn"
+    );
+    expect(response).toEqual({ status: 500 });
+  });
+});
+
+describe("tagS3Object", () => {
+  test("successfully tags", async () => {
+    mockS3Client.on(PutObjectTaggingCommand).resolvesOnce({ VersionId: "yeet" });
+    const input = {
+      Bucket: "foo",
+      Key: "bar",
+      Tagging: {
+        TagSet: [{ Key: "some-tag", Value: "some-value" }],
+      },
+    };
+    const response = await tagS3Object(mockS3Client, { Bucket: "foo", Key: "bar" }, [
+      { Key: "some-tag", Value: "some-value" },
+    ]);
+    expect(response).toBe(true);
+    expect(mockS3Client).toHaveReceivedCommandWith(PutObjectTaggingCommand, input);
+  });
+
+  test("fails to tag", async () => {
+    mockS3Client.on(PutObjectTaggingCommand).resolvesOnce({});
+    const response = await tagS3Object(mockS3Client, {}, []);
+    expect(response).toBe(false);
+  });
+});
