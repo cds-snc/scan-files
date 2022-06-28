1bfabfccf1913ece3650914345998bb0d3dd611c
Pat Heard <patrick.heard@cds-snc.ca>
2022-06-28 18:25:10 +0000

    fix: typo in KMS resource name

--- terragrunt/aws/api/secrets.tf
+++ terragrunt/aws/api/secrets.tf
@@ -1,6 +1,6 @@
 resource "aws_secretsmanager_secret" "api_auth_token" {
   name       = "/scan-files/api_auth_token"
-  kms_key_id = aws_kms_key.scan_files.arn
+  kms_key_id = aws_kms_key.scan-files.arn
 
   tags = {
     CostCentre = var.billing_code
