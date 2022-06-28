d8250ed05c6a61f48c16c7f88fd8c67d1767db38
Mohamed Nur <85885638+mohamed-cds@users.noreply.github.com>
2022-06-28 10:33:27 -0400

    fix: provide viewer certificate (#127)

--- terragrunt/aws/api/cloudfront.tf
+++ terragrunt/aws/api/cloudfront.tf
@@ -35,6 +35,7 @@ resource "aws_cloudfront_distribution" "scan_files_api" {
   }
 
   viewer_certificate {
+    acm_certificate_arn            = aws_acm_certificate_validation.scan_files_certificate_validation.certificate_arn
     cloudfront_default_certificate = false
     minimum_protocol_version       = "TLSv1.2_2021"
   }
