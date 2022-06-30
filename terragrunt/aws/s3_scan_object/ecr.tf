af058d1590a4b2d5b4def2c54ca6c6b1e98f3ca3
Pat Heard <patrick.heard@cds-snc.ca>
2022-06-30 17:48:49 +0000

    fix: S3 scan object ECR tag policy
    Update the ECR policy to only keep the latest 20 images.  This
    simplifies things since there are never any untagged images in
    the repository anyway.

--- terragrunt/aws/s3_scan_object/ecr.tf
+++ terragrunt/aws/s3_scan_object/ecr.tf
@@ -80,22 +80,9 @@ resource "aws_ecr_lifecycle_policy" "s3_scan_object_exire_untagged" {
     "rules" : [
       {
         "rulePriority" : 1,
-        "description" : "Expire untagged images older than 14 days",
+        "description" : "Keep last 20 images",
         "selection" : {
-          "tagStatus" : "untagged",
-          "countType" : "sinceImagePushed",
-          "countUnit" : "days",
-          "countNumber" : 14
-        },
-        "action" : {
-          "type" : "expire"
-        }
-      },
-      {
-        "rulePriority" : 2,
-        "description" : "Keep last 20 tagged images",
-        "selection" : {
-          "tagStatus" : "tagged",
+          "tagStatus" : "any",
           "countType" : "imageCountMoreThan",
           "countNumber" : 20
         },
