900098edc62a84cabbf8fb606048573292e9c8d8
Pat Heard <patrick.heard@cds-snc.ca>
2022-08-31 17:29:58 +0000

    fix: quotes

--- terragrunt/aws/alarms/sql/athena_waf_all_requests.sql
+++ terragrunt/aws/alarms/sql/athena_waf_all_requests.sql
@@ -2,6 +2,6 @@ SELECT
     date_format(from_unixtime(timestamp / 1000e0), '%Y-%m-%d %T') as time, 
     httprequest.httpmethod, httprequest.uri, label.name, action, labels, httprequest
 FROM 
-    '${database_name}.${table_name}',
+    `${database_name}.${table_name}`,
     UNNEST(labels) as t(label)
 ORDER BY label.name, uri;
