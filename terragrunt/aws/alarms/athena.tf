515518d68db4a28c7c16b0de839abc76f6546d44
Pat Heard <patrick.heard@cds-snc.ca>
2022-08-31 17:11:22 +0000

    feat: add Athena database for WAF access logs
    Add an Athena database and saved create table query to allow
    for easy analysis of WAF ACL access logs.

---
+++ terragrunt/aws/alarms/athena.tf
@@ -0,0 +1,67 @@
+#
+# Athena database and query to view
+# WAF ACL access logs
+#
+resource "aws_athena_database" "logs" {
+  name   = "${var.product_name}-${var.env}"
+  bucket = module.athena_bucket.s3_bucket_id
+
+  encryption_configuration {
+    encryption_option = "SSE_S3"
+  }
+}
+
+resource "aws_athena_workgroup" "primary" {
+  name = "primary"
+
+  configuration {
+    enforce_workgroup_configuration    = true
+    publish_cloudwatch_metrics_enabled = true
+
+    result_configuration {
+      output_location = "s3://${module.athena_bucket.s3_bucket_id}"
+
+      encryption_configuration {
+        encryption_option = "SSE_S3"
+      }
+    }
+  }
+
+  tags = {
+    CostCentre = var.billing_code
+    Terraform  = "true"
+  }
+}
+
+resource "aws_athena_named_query" "create_table_waf_logs" {
+  name      = "Create WAF table"
+  workgroup = aws_athena_workgroup.primary.name
+  database  = aws_athena_database.logs.name
+  query = templatefile("${path.module}/sql/athena_create_waf_table.sql",
+    {
+      bucket_location = "s3://${var.cbs_satellite_bucket_name}/waf_acl_logs/AWSLogs/${var.account_id}/"
+      database_name   = aws_athena_database.logs.name
+      table_name      = "waf_logs"
+    }
+  )
+}
+
+#
+# Hold the Athena data
+#
+module "athena_bucket" {
+  source            = "github.com/cds-snc/terraform-modules?ref=v3.0.13//S3"
+  bucket_name       = "${var.product_name}-${var.env}-athena-bucket"
+  billing_tag_value = var.billing_code
+
+  lifecycle_rule = [
+    {
+      id      = "expire-objects-after-7-days"
+      enabled = true
+      expiration = {
+        days                         = 7
+        expired_object_delete_marker = false
+      }
+    },
+  ]
+}
