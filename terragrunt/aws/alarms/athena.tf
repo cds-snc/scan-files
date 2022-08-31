64cbcfc2e7be8805e73e582dfea8db1c33f8ec38
Pat Heard <patrick.heard@cds-snc.ca>
2022-08-31 17:26:09 +0000

    feat: add more saved queries

--- terragrunt/aws/alarms/athena.tf
+++ terragrunt/aws/alarms/athena.tf
@@ -33,15 +33,39 @@ resource "aws_athena_workgroup" "primary" {
   }
 }
 
-resource "aws_athena_named_query" "create_table_waf_logs" {
-  name      = "Create WAF table"
+resource "aws_athena_named_query" "waf_create_table" {
+  name      = "WAF: create table"
   workgroup = aws_athena_workgroup.primary.name
   database  = aws_athena_database.logs.name
-  query = templatefile("${path.module}/sql/athena_create_waf_table.sql",
+  query = templatefile("${path.module}/sql/athena_waf_create_table.sql",
     {
       bucket_location = "s3://${var.cbs_satellite_bucket_name}/waf_acl_logs/AWSLogs/${var.account_id}/"
       database_name   = aws_athena_database.logs.name
       table_name      = "waf_logs"
+    }
+  )
+}
+
+resource "aws_athena_named_query" "waf_blocked_requests" {
+  name      = "WAF: blocked requests"
+  workgroup = aws_athena_workgroup.primary.name
+  database  = aws_athena_database.logs.name
+  query = templatefile("${path.module}/sql/athena_waf_blocked_requests.sql",
+    {
+      database_name = aws_athena_database.logs.name
+      table_name    = "waf_logs"
+    }
+  )
+}
+
+resource "aws_athena_named_query" "waf_all_requests" {
+  name      = "WAF: all requests"
+  workgroup = aws_athena_workgroup.primary.name
+  database  = aws_athena_database.logs.name
+  query = templatefile("${path.module}/sql/athena_waf_all_requests.sql",
+    {
+      database_name = aws_athena_database.logs.name
+      table_name    = "waf_logs"
     }
   )
 }
