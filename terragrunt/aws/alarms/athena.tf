5c2b8295cbd470d036330a0198e586b01295aac8
Pat Heard <patrick.heard@cds-snc.ca>
2022-09-06 16:58:14 +0000

    feat: use Athena terraform module
    Switch to using the new Athena TF module to create the
    database, workgroup and queries.

--- terragrunt/aws/alarms/athena.tf
+++ terragrunt/aws/alarms/athena.tf
@@ -1,8 +1,18 @@
+module "simple" {
+  source = "github.com/cds-snc/terraform-modules?ref=v3.0.16//athena_access_logs"
+
+  athena_bucket_name         = module.athena_bucket.s3_bucket_id
+  waf_access_queries_create  = true
+  waf_access_log_bucket_name = var.cbs_satellite_bucket_name
+
+  billing_tag_value = var.billing_code
+}
+
 #
 # Hold the Athena data
 #
 module "athena_bucket" {
-  source            = "github.com/cds-snc/terraform-modules?ref=v3.0.13//S3"
+  source            = "github.com/cds-snc/terraform-modules?ref=v3.0.16//S3"
   bucket_name       = "${var.product_name}-${var.env}-athena-bucket"
   billing_tag_value = var.billing_code
 
