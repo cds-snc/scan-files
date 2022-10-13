ba9d4f6a0527cdde34d145da669c8915b079cdad
Pat Heard <patrick.heard@cds-snc.ca>
2022-10-13 17:15:33 +0000

    test: single pool connection
    Update the SQLAlchemey database engine to only have a single pool
    connection. We are using RDS proxy, which handles connection
    pooling for us.

--- api/database/db.py
+++ api/database/db.py
@@ -2,6 +2,7 @@ import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
 
 if os.environ.get("CI"):
     connection_string = os.environ.get("SQLALCHEMY_DATABASE_TEST_URI")
@@ -11,10 +12,8 @@ else:
 db_engine = create_engine(
     connection_string,
     connect_args={"connect_timeout": 10},
-    pool_size=1,
-    pool_pre_ping=True,  # Check that a connection is still active before attempting to use
-    pool_recycle=1500,  # Prune connections older than 25 minutes (RDS Proxy has a timeout of 30 minutes)
-    pool_use_lifo=True,  # Re-use last connection used (allows server-side timeouts to remove unused connections)
+    poolclass=StaticPool,
+    pool_recycle=1500,
 )
 db_session = sessionmaker(bind=db_engine)
 
