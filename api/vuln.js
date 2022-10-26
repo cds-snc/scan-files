cfae8a29c2dc088efc44577214b22859eaf4ab4c
Alvaro Scheid <ascheid@gmail.com>
2022-10-26 10:46:32 -0400

    test: code injection

--- api/vuln.js
+++ api/vuln.js
@@ -13,3 +13,24 @@ function checkLogin(req, db) {
       return result.length !== 0;
     });
   }
+
+  const express = require('express')
+  var pug = require('pug');
+  const app = express()
+  
+  app.post('/', (req, res) => {
+      var input = req.query.username;
+      var template = `
+  doctype
+  html
+  head
+      title= 'Hello world'
+  body
+      form(action='/' method='post')
+          input#name.form-control(type='text)
+          button.btn.btn-primary(type='submit') Submit
+      p Hello `+ input
+      var fn = pug.compile(template);
+      var html = fn();
+      res.send(html);
+  })
