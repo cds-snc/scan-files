29451338b87c43ece699256d2116f85a24c63417
Pat Heard <patrick.heard@cds-snc.ca>
2022-11-03 20:55:14 +0000

    fix: replace unsupported method
    `OPTIONS` method is not supported by AWS.

--- terragrunt/aws/api/lambda.tf
+++ terragrunt/aws/api/lambda.tf
@@ -97,7 +97,7 @@ resource "aws_lambda_function_url" "scan_files_url" {
   cors {
     allow_credentials = true
     allow_origins     = ["https://${var.domain}"]
-    allow_methods     = ["GET", "POST", "OPTIONS"]
+    allow_methods     = ["GET", "POST", "HEAD"]
     max_age           = 86400
   }
 }
