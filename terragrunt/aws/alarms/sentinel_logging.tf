75ab8bf5a020759e519acc1651d8f8bcf556372f
Max Neuvians <max@neuvians.net>
2022-11-23 08:48:47 -0500

    feat: Send scan verdict output to Sentinel (#429)
    * feat: Send scan verdict output to Sentinel
    
    * fix: var
    
    * fix: arn
    
    * fix:typo

---
+++ terragrunt/aws/alarms/sentinel_logging.tf
@@ -0,0 +1,23 @@
+locals {
+  scan_verdict_suspicious_arn = "arn:aws:logs:${var.region}:${var.account_id}:log-group:${var.scan_files_api_log_group_name}"
+}
+
+module "sentinel_forwarder" {
+  source            = "github.com/cds-snc/terraform-modules?ref=v4.0.2//sentinel_forwarder"
+  function_name     = "sentinel-cloud-watch-forwarder"
+  billing_tag_value = var.billing_code
+
+  customer_id = var.sentinel_customer_id
+  shared_key  = var.sentinel_shared_key
+
+  cloudwatch_log_arns = [local.scan_verdict_suspicious_arn]
+}
+
+
+resource "aws_cloudwatch_log_subscription_filter" "scan_verdict_suspicious" {
+  name            = local.scan_verdict_suspicious
+  log_group_name  = var.scan_files_api_log_group_name
+  filter_pattern  = "?suspicious ?malicious ?unknown ?unable_to_scan"
+  destination_arn = module.sentinel_forwarder.lambda_arn
+  distribution    = "Random"
+}
