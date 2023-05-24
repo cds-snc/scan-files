c988a63f8da757280bdb69c5ef05ecdecb6dbe9b
Pat Heard <patrick.heard@cds-snc.ca>
2023-05-24 19:57:41 +0000

    chore: bump to latest module version

--- terragrunt/aws/integration_test/s3_scan_object.tf
+++ terragrunt/aws/integration_test/s3_scan_object.tf
@@ -1,11 +1,13 @@
 module "integration_test" {
-  source = "github.com/cds-snc/terraform-modules?ref=v3.0.10//S3_scan_object"
+  source = "github.com/cds-snc/terraform-modules?ref=v5.1.11//S3_scan_object"
 
   product_name                = "integration-test"
   s3_upload_bucket_name       = module.integration_test_bucket.s3_bucket_id
   scan_files_role_arn         = "arn:aws:iam::${var.account_id}:role/scan-files-api"
   s3_scan_object_function_arn = "arn:aws:lambda:ca-central-1:${var.account_id}:function:s3-scan-object"
   s3_scan_object_role_arn     = "arn:aws:iam::${var.account_id}:role/s3-scan-object"
+
+  log_level = "DEBUG"
 
   billing_tag_value = var.billing_code
 }
