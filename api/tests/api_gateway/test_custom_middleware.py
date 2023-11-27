7697208bf918f36b7795cd4965fd7623461ab3a6
Pat Heard <patrick.heard@cds-snc.ca>
2023-11-27 12:53:42 -0500

    fix: improve API secret retrieval validation (#784)
    1. Update the API's entry script to more carefully check if the secrets
    have been correctly retrieved.
    2. Update the API's token verification middleware to throw an error if
    there's no API_AUTH_TOKEN during init.
    3. Update the verify_token function to log a redacted output of the failed
    tokens when verification fails.

---
+++ api/tests/api_gateway/test_custom_middleware.py
@@ -0,0 +1,8 @@
+from api_gateway import custom_middleware
+
+
+def test_redact():
+    assert custom_middleware.redact("1234567890") == "****567890"
+    assert custom_middleware.redact("123456") == "123456"
+    assert custom_middleware.redact(123456) == 123456
+    assert custom_middleware.redact(None) is None
