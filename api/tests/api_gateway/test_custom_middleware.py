2c2252e3a3c070b87b2fb2479bd3b7c8eeca122e
Pat Heard <patrick.heard@cds-snc.ca>
2023-11-27 16:50:17 +0000

    fix: make lint

--- api/tests/api_gateway/test_custom_middleware.py
+++ api/tests/api_gateway/test_custom_middleware.py
@@ -5,4 +5,4 @@ def test_redact():
     assert custom_middleware.redact("1234567890") == "****567890"
     assert custom_middleware.redact("123456") == "123456"
     assert custom_middleware.redact(123456) == 123456
-    assert custom_middleware.redact(None) == None
+    assert custom_middleware.redact(None) is None
