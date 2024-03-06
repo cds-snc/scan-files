84d116203e94b9543ea444bc28a30cf46ffe13a4
Pat Heard <patrick.heard@cds-snc.ca>
2024-03-06 14:01:51 -0500

    feat: add `api` module outputs for the provisioned Lambda function (#886)
    Update the outputs so that the sync function values are also made available
    to dependencies.  As part of this change, the original function's outputs
    were renamed to make it clear which function they referenced.

--- terragrunt/aws/api/outputs.tf
+++ terragrunt/aws/api/outputs.tf
@@ -2,6 +2,10 @@ output "api_cloudfront_distribution_id" {
   value = aws_cloudfront_distribution.scan_files["api"].id
 }
 
+output "api_sync_cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.scan_files["api-provisioned"].id
+}
+
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
@@ -14,29 +18,50 @@ output "log_bucket_id" {
   value = module.log_bucket.s3_bucket_id
 }
 
-output "function_arn" {
+output "api_function_arn" {
   value = module.scan_files["api"].function_arn
 }
 
-output "function_log_group_name" {
+output "api_sync_function_arn" {
+  value = module.scan_files["api-provisioned"].function_arn
+}
+
+output "api_function_log_group_name" {
   value = "/aws/lambda/${module.scan_files["api"].function_name}"
 }
 
-output "function_name" {
+output "api_sync_function_log_group_name" {
+  value = "/aws/lambda/${module.scan_files["api-provisioned"].function_name}"
+}
+
+output "api_function_name" {
   value = module.scan_files["api"].function_name
 }
 
-output "function_role_arn" {
+output "api_sync_function_name" {
+  value = module.scan_files["api-provisioned"].function_name
+}
+
+output "api_function_role_arn" {
   value = "arn:aws:iam::${var.account_id}:role/${module.scan_files["api"].function_name}"
 }
 
-output "function_url" {
+output "api_function_url" {
   value     = aws_lambda_function_url.scan_files["api"].function_url
   sensitive = true
 }
 
-output "invoke_arn" {
+output "api_sync_function_url" {
+  value     = aws_lambda_function_url.scan_files["api-provisioned"].function_url
+  sensitive = true
+}
+
+output "api_invoke_arn" {
   value = module.scan_files["api"].invoke_arn
+}
+
+output "api_sync_invoke_arn" {
+  value = module.scan_files["api-provisioned"].invoke_arn
 }
 
 output "route53_health_check_api_id" {
