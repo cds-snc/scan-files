37161dcb9a83f1d176c6a4940ef60cc7f0e68d34
Pat Heard <patrick.heard@cds-snc.ca>
2024-08-25 19:57:27 +0000

    fix: add missing detect_verdict unit tests

--- api/tests/clamav/test_scan.py
+++ api/tests/clamav/test_scan.py
@@ -1,8 +1,9 @@
 from unittest.mock import patch, ANY, MagicMock
-from clamav_scanner.common import AV_SIGNATURE_OK
+from clamav_scanner.clamav import determine_verdict
+from clamav_scanner.common import AV_SIGNATURE_OK, AV_SIGNATURE_UNKNOWN
 from clamav_scanner.scan import launch_scan, sns_scan_results
 from factories import ScanFactory
-from models.Scan import ScanVerdicts
+from models.Scan import ScanProviders, ScanVerdicts
 
 
 @patch("clamav_scanner.scan.log")
@@ -126,3 +127,71 @@ def test_sns_scan_results_error(mock_db_session, mock_aws_session, session):
             "aws-account": {"DataType": "String", "StringValue": "210987654321"},
         },
     )
+
+
+@patch("clamav_scanner.clamav.log")
+def test_determine_verdict_clamav_clean(mock_log):
+    provider = ScanProviders.CLAMAV.value
+    path = "/test/path"
+    summary = {path: "clean", "Time": "1.000"}
+    av_proc = MagicMock()
+    av_proc.returncode = 0
+
+    verdict, signature = determine_verdict(provider, path, summary, av_proc)
+    assert verdict == ScanVerdicts.CLEAN.value
+    assert signature == "clean"
+
+
+@patch("clamav_scanner.clamav.log")
+def test_determine_verdict_clamav_false_positive(mock_log):
+    provider = ScanProviders.CLAMAV.value
+    path = "/test/path"
+    false_positive_signature = "PUA.Pdf.Trojan.EmbeddedJavaScript-1 FOUND"
+    summary = {path: false_positive_signature, "Time": "1.000"}
+    av_proc = MagicMock()
+    av_proc.returncode = 1
+
+    verdict, signature = determine_verdict(provider, path, summary, av_proc)
+    assert verdict == ScanVerdicts.CLEAN.value
+    assert signature == false_positive_signature
+
+
+@patch("clamav_scanner.clamav.log")
+def test_determine_verdict_clamav_malicious(mock_log):
+    provider = ScanProviders.CLAMAV.value
+    path = "/test/path"
+    summary = {path: "malicious", "Time": "1.000"}
+    av_proc = MagicMock()
+    av_proc.returncode = 1
+
+    verdict, signature = determine_verdict(provider, path, summary, av_proc)
+    assert verdict == ScanVerdicts.MALICIOUS.value
+    assert signature == "malicious"
+
+
+@patch("clamav_scanner.clamav.log")
+def test_determine_verdict_clamav_unable_to_scan(mock_log):
+    provider = ScanProviders.CLAMAV.value
+    path = "/test/path"
+    summary = {path: "unknown", "Time": "0.000"}
+    av_proc = MagicMock()
+    av_proc.returncode = 0
+
+    verdict, signature = determine_verdict(provider, path, summary, av_proc)
+    assert verdict == ScanVerdicts.UNABLE_TO_SCAN.value
+    assert signature == AV_SIGNATURE_UNKNOWN
+    mock_log.error.assert_called()
+
+
+@patch("clamav_scanner.clamav.log")
+def test_determine_verdict_clamav_error(mock_log):
+    provider = ScanProviders.CLAMAV.value
+    path = "/test/path"
+    summary = {path: "unknown", "Time": "1.000"}
+    av_proc = MagicMock()
+    av_proc.returncode = 2
+
+    verdict, signature = determine_verdict(provider, path, summary, av_proc)
+    assert verdict == ScanVerdicts.ERROR.value
+    assert signature == AV_SIGNATURE_UNKNOWN
+    mock_log.error.assert_called()
